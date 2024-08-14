ebaa82d59bbc952ef33897b65e32470d07dfdec0
Ital023 <contadoitalo911@gmail.com>
2024-08-14 20:52:23 -0300

    feat: Not Found Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ClientHome from "./routes/ClientHome";
 import Catalog from "./routes/ClientHome/Catalog";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
@@ -7,11 +7,17 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<ClientHome />}>
           <Route index element={<Catalog />} />
           <Route path="catalog" element={<Catalog />} />
-          <Route path="product-details/:productId" element={<ProductDetails />} />
+          <Route
+            path="product-details/:productId"
+            element={<ProductDetails />}
+          />
         </Route>
+
+        <Route path="*" element={<Navigate to={"/"} />} />
       </Routes>
     </BrowserRouter>
   );
