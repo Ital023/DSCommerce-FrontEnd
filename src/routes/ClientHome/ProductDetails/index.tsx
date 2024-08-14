32865fd92178a0bbf9ab142db23fc01d665c80c1
Ital023 <contadoitalo911@gmail.com>
2024-08-14 20:04:18 -0300

    feat: Links, catalog, product details

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -4,21 +4,22 @@ import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 export default function ProductDetails() {
-
   const params = useParams();
   const product = productService.findById(Number(params.productId));
 
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        {
-          product && <ProductDetailsCard product={product} />
-        }
+        {product && <ProductDetailsCard product={product} />}
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
-          <ButtonInverse text="Início" />
+
+          <Link to={"/"}>
+            <ButtonInverse text="Início" />
+          </Link>
         </div>
       </section>
     </main>
