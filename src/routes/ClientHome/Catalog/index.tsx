1edfb0fbff0a4201118f4404c1e731c6e606e77d
Ital023 <contadoitalo911@gmail.com>
2024-08-14 18:05:27 -0300

    feat: Adding React Router DOM

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -1,9 +1,8 @@
 import "./styles.css";
-import HeaderClient from "../../components/HeaderClient";
-import SearchBar from "../../components/SearchBar";
-import CatalogCard from "../../components/CatalogCard";
-import ButtonNextPage from "../../components/ButtonNextPage";
-import { ProductDTO } from "../../models/product";
+import SearchBar from "../../../components/SearchBar";
+import CatalogCard from "../../../components/CatalogCard";
+import ButtonNextPage from "../../../components/ButtonNextPage";
+import { ProductDTO } from "../../../models/product";
 
 const product: ProductDTO = {
   id: 2,
@@ -30,8 +29,6 @@ const product: ProductDTO = {
 
 export default function Catalog() {
   return (
-    <>
-      <HeaderClient />
       <main>
         <section id="catalog-section" className="dsc-container">
           <SearchBar />
@@ -53,6 +50,5 @@ export default function Catalog() {
           <ButtonNextPage />
         </section>
       </main>
-    </>
   );
 }
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,9 +1,9 @@
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import { ProductDTO } from "../../../models/product";
 import "./styles.css";
-import ButtonInverse from "../../components/ButtonInverse";
-import ButtonPrimary from "../../components/ButtonPrimary";
-import HeaderClient from "../../components/HeaderClient";
-import ProductDetailsCard from "../../components/ProductDetailsCard";
-import { ProductDTO } from "../../models/product";
+
 
 const product: ProductDTO = {
   id: 2,
@@ -30,8 +30,6 @@ const product: ProductDTO = {
 
 export default function ProductDetails() {
   return (
-    <>
-      <HeaderClient />
       <main>
         <section id="product-details-section" className="dsc-container">
           <ProductDetailsCard product={product} />
@@ -41,6 +39,5 @@ export default function ProductDetails() {
           </div>
         </section>
       </main>
-    </>
   );
 }
