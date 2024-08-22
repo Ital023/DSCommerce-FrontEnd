7de64a19ee21bce67f93cac45f8592ed5b769429
Ital023 <contadoitalo911@gmail.com>
2024-08-21 22:55:59 -0300

    feat: Searchbar onSearch event

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,11 +1,38 @@
-import './styles.css'
+import { useState } from "react";
+import "./styles.css";
 
-export default function SearchBar() {
+type Props = {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  onSearch: Function;
+};
+
+export default function SearchBar({ onSearch }: Props) {
+  
+  const[text, setText] = useState("");
+
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleChange(event: any) {
+    event.preventDefault();
+    setText(event.target.value);
+  }
+
+  function handleResetClick() {
+    setText("");
+    onSearch(text);
+  }
+  
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    onSearch(text);
+  }
+
   return (
-    <form className="dsc-search-bar">
+    <form className="dsc-search-bar" onSubmit={handleSubmit}>
       <button type="submit">🔎︎</button>
-      <input type="text" placeholder="Nome do produto" />
-      <button type="reset">🗙</button>
+      <input type="text" value={text} onChange={handleChange} placeholder="Nome do produto" />
+      <button onClick={handleResetClick}>🗙</button>
     </form>
   );
 }
